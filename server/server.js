7beb170d94f1d36b1947f16a88573e35acab6683
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-12 21:16:25 +0100

    Edit db connection in 'server.js'

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 // Import project files
 import { config } from "./src/config/app-config.js";
-// import Database from "./src/database/connection.js";
+import db from "./src/database/connection.js";
 import app from "./src/app.js";
 
 // DB Connection
-// const db = new Database();
-// const connPromise = db.connect();
-// connPromise
-//   .then((result) => console.log(result))
-//   .catch((error) => console.log(error));
+db.connect();
 
 // Run server
 app.listen(config().app.PORT, () => {
