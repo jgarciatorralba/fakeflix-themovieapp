d0a0320f85df3c272f0801c202d3d0615cee825e
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-12 18:09:14 +0100

    Edit 'app-config.js' to use updated '.env' variables

---
+++ server/src/config/app-config.js
@@ -0,0 +1,58 @@
+// Import dependencies
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = () => {
+  const MODE = process.env.NODE_ENV || "production";
+
+  const config = {
+    test: {
+      MODE: "test",
+      PORT: Number(process.env.PORT),
+      API_KEY: process.env.API_KEY,
+      API_TOKEN: process.env.API_TOKEN,
+      SECRET: process.env.SECRET,
+      SERVER_DOMAIN: process.env.SERVER_DOMAIN,
+      CLIENT_DOMAIN: process.enf.CLIENT_DOMAIN,
+      db: {
+        CLUSTER: process.env.DB_CLUSTER,
+        USER: process.env.DB_USER,
+        PWD: process.env.DB_PWD,
+        NAME: process.env.DB_NAME,
+      },
+    },
+    production: {
+      MODE: "production",
+      PORT: Number(process.env.PORT),
+      API_KEY: process.env.API_KEY,
+      API_TOKEN: process.env.API_TOKEN,
+      SECRET: process.env.SECRET,
+      SERVER_DOMAIN: process.env.SERVER_DOMAIN,
+      CLIENT_DOMAIN: process.enf.CLIENT_DOMAIN,
+      db: {
+        CLUSTER: process.env.DB_CLUSTER,
+        USER: process.env.DB_USER,
+        PWD: process.env.DB_PWD,
+        NAME: process.env.DB_NAME,
+      },
+    },
+    development: {
+      MODE: "development",
+      PORT: Number(process.env.PORT),
+      API_KEY: process.env.API_KEY,
+      API_TOKEN: process.env.API_TOKEN,
+      SECRET: process.env.SECRET,
+      SERVER_DOMAIN: process.env.SERVER_DOMAIN,
+      CLIENT_DOMAIN: process.enf.CLIENT_DOMAIN,
+      db: {
+        CLUSTER: process.env.DB_CLUSTER,
+        USER: process.env.DB_USER,
+        PWD: process.env.DB_PWD,
+        NAME: process.env.DB_NAME,
+      },
+    },
+  };
+
+  return config[MODE];
+};
