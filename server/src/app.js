66619b0019f3fac870e433e798d5f3b04c4c6a73
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-16 18:35:25 +0100

    Add routers for comments, likes, dislikes and favs

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,10 @@ import cors from "cors";
 import { config } from "./config/app-config.js";
 import authRouter from "./routers/authRouter.js";
 import userRouter from "./routers/userRouter.js";
+import commentRouter from "./routers/commentRouter.js";
+import likeRouter from "./routers/likeRouter.js";
+import dislikeRouter from "./routers/dislikeRouter.js";
+import favouriteRouter from "./routers/favouriteRouter.js";
 
 const app = express();
 
@@ -19,6 +23,10 @@ app.use(express.json());
 // Routers
 app.use("/", authRouter);
 app.use("/user", userRouter);
+app.use("/comment", commentRouter);
+app.use("/like", likeRouter);
+app.use("/dislike", dislikeRouter);
+app.use("/favourite", favouriteRouter);
 
 // Allow "public" folder to serve static files
 app.use(express.static(path.resolve() + "/server/public"));
