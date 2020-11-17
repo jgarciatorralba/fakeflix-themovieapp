f4cb5c2cc0740986883b6200897178c80214116e
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-17 19:38:08 +0100

    Complete testing for userRouter

---
+++ server/src/routers/__tests__/userRouter.test.js
@@ -0,0 +1,102 @@
+// Import dependencies
+import express from "express";
+import request from "supertest";
+
+// Import router
+import userRouter from "../userRouter.js";
+
+// Import controller
+import userController from "../../controllers/userController.js";
+
+// Instance of express app ("fake" express app)
+const app = express();
+app.use(express.json());
+
+// Route
+app.use("/user-test", userRouter);
+
+let spyMethodFindById = null;
+let spyMethodFindOnlyDeletedById = null;
+let spyMethodUpdateUserById = null;
+let spyMethodDeleteUser = null;
+
+describe("Testing user routes", () => {
+  beforeAll(async () => {
+    userController.findById = jest.fn().mockResolvedValue({
+      _id: { $oid: "5fb28e6337257c23175321ad" },
+      email: "jest-test@emaildomain.com",
+      username: "jest-test",
+      avatar: "default.jpg",
+    });
+
+    userController.findOnlyDeletedById = jest.fn().mockResolvedValue(null);
+    userController.updateUserById = jest.fn().mockResolvedValue(null);
+    userController.deleteUser = jest.fn().mockResolvedValue(null);
+
+    spyMethodFindById = jest.spyOn(userController, "findById");
+    spyMethodFindOnlyDeletedById = jest.spyOn(
+      userController,
+      "findOnlyDeletedById"
+    );
+    spyMethodUpdateUserById = jest.spyOn(userController, "updateUserById");
+    spyMethodDeleteUser = jest.spyOn(userController, "deleteUser");
+  });
+
+  afterAll(() => {
+    spyMethodFindById.mockClear();
+    spyMethodFindOnlyDeletedById.mockClear();
+    spyMethodUpdateUserById.mockClear();
+    spyMethodDeleteUser.mockClear();
+  });
+
+  test("GET - get an existing user", async () => {
+    expect.assertions(3);
+
+    const response = await request(app)
+      .get("/user-test")
+      .set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYjI4ZmU1NzBmZDg5MjQwNmFmM2YwOSIsImlhdCI6MTYwNTUzNzc5OH0.3u9o6DTXLexUDV8nSHqsRp32d42R6m3VawNIdjssxY4"
+      );
+    expect(response.status).toEqual(200);
+    expect(userController.findById).toHaveBeenCalledTimes(1);
+    expect(JSON.parse(response.text)).toMatchObject({ error: null, data: {} });
+  });
+
+  test("PATCH - Update an existing user", async () => {
+    expect.assertions(4);
+
+    const response = await request(app)
+      .patch("/user-test")
+      .set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYjI4ZmU1NzBmZDg5MjQwNmFmM2YwOSIsImlhdCI6MTYwNTUzNzc5OH0.3u9o6DTXLexUDV8nSHqsRp32d42R6m3VawNIdjssxY4"
+      );
+
+    expect(response.status).toEqual(200);
+    expect(userController.findOnlyDeletedById).toHaveBeenCalledTimes(1);
+    expect(userController.updateUserById).toHaveBeenCalledTimes(1);
+    expect(JSON.parse(response.text)).toMatchObject({
+      error: null,
+      data: "User data updated!",
+    });
+  });
+
+  test("DELETE - Delete existing user", async () => {
+    expect.assertions(3);
+
+    const response = await request(app)
+      .delete("/user-test")
+      .set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYjI4ZmU1NzBmZDg5MjQwNmFmM2YwOSIsImlhdCI6MTYwNTUzNzc5OH0.3u9o6DTXLexUDV8nSHqsRp32d42R6m3VawNIdjssxY4"
+      );
+
+    expect(response.status).toEqual(200);
+    expect(userController.deleteUser).toHaveBeenCalledTimes(1);
+    expect(JSON.parse(response.text)).toMatchObject({
+      error: null,
+      data: "User deleted!",
+    });
+  });
+});
