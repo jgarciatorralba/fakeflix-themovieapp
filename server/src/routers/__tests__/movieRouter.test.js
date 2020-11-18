242f6c6dbea894ae7335e06dfb8106c5bcf45a2f
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-18 19:46:51 +0100

    Add test to get favourite movies in 'movieRouter.test.js'

--- server/src/routers/__tests__/movieRouter.test.js
+++ server/src/routers/__tests__/movieRouter.test.js
@@ -7,6 +7,7 @@ import movieRouter from "../movieRouter.js";
 
 // Import controller
 import movieController from "../../controllers/movieController.js";
+import favouriteController from "../../controllers/favouriteController.js";
 
 // Instance of express app ("fake" express app)
 const app = express();
@@ -17,19 +18,26 @@ app.use("/movie-test", movieRouter);
 
 let spyMethodGetConfig = null;
 let spyMethodGetRandomMovie = null;
+let spyMethodGetUserFavourites = null;
 
 describe("Testing movie routes...", () => {
   beforeAll(async () => {
     movieController.getConfig = jest.fn().mockResolvedValue({});
     movieController.getRandomMovie = jest.fn().mockResolvedValue({});
+    favouriteController.getUserFavourites = jest.fn().mockResolvedValue([]);
 
     spyMethodGetConfig = jest.spyOn(movieController, "getConfig");
     spyMethodGetRandomMovie = jest.spyOn(movieController, "getRandomMovie");
+    spyMethodGetUserFavourites = jest.spyOn(
+      favouriteController,
+      "getUserFavourites"
+    );
   });
 
   afterAll(async () => {
     spyMethodGetConfig.mockClear();
     spyMethodGetRandomMovie.mockClear();
+    spyMethodGetUserFavourites.mockClear();
   });
 
   test("GET - get configuration details", async () => {
@@ -60,5 +68,20 @@ describe("Testing movie routes...", () => {
     expect(response.status).toEqual(200);
     expect(movieController.getRandomMovie).toHaveBeenCalledTimes(1);
     expect(JSON.parse(response.text)).toMatchObject({ error: null, data: {} });
+  });
+
+  test("GET - get favourite movies for a given user", async () => {
+    expect.assertions(3);
+
+    const response = await request(app)
+      .get("/movie-test/favourites")
+      .set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYjI4ZmU1NzBmZDg5MjQwNmFmM2YwOSIsImlhdCI6MTYwNTUzNzc5OH0.3u9o6DTXLexUDV8nSHqsRp32d42R6m3VawNIdjssxY4"
+      );
+
+    expect(response.status).toEqual(200);
+    expect(favouriteController.getUserFavourites).toHaveBeenCalledTimes(1);
+    expect(JSON.parse(response.text)).toMatchObject({ error: null, data: [] });
   });
 });
