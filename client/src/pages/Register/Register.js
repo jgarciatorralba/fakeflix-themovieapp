87c0e1adadc1882935e7339cc0f9b2ea728e00a3
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-26 15:04:13 +0100

    Create page Register

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,10 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import Alert from "react-bootstrap/Alert";
+
+import Logo from "../../components/Logo/Logo";
+import Label from "../../components/Label/Label";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+import Footer from "../../components/Footer/Footer";
+import HelperParagraph from "../../components/HelperParagraph/HelperParagraph";
+
+import "./Register.scss";
 
 function Register() {
+  const [show, setShow] = useState(true);
+
   return (
-    <div className="Register">
-      <h1>Register</h1>
-    </div>
+    <main className="container cont-register">
+      <div className="text-center w-100">
+        <Logo fontSize="2.75rem" />
+
+        <form className="form-register" autoComplete="off">
+          <h1 className="h3 mb-3 font-weight-normal">User registration</h1>
+
+          <Label htmlFor="registerName">Username</Label>
+          <Input
+            htmlType="text"
+            id="registerName"
+            name="registerName"
+            additionalClasses="upper"
+            placeholder="Username"
+            required
+            autoFocus
+          />
+
+          <Label htmlFor="registerEmail">Email address</Label>
+          <Input
+            htmlType="email"
+            id="registerEmail"
+            name="registerEmail"
+            additionalClasses="mid"
+            placeholder="Email address"
+            required
+          />
+
+          <Label htmlFor="registerPassword">Password</Label>
+          <Input
+            htmlType="password"
+            id="registerPassword"
+            name="registerPassword"
+            additionalClasses="lower"
+            placeholder="Password"
+            minLength="6"
+            required
+          />
+
+          {show && (
+            <Alert
+              show={true}
+              onClose={() => setShow(false)}
+              variant="danger"
+              closeLabel="Close Alert"
+              dismissible
+              fade="true"
+            >
+              Dismissible alert!
+            </Alert>
+          )}
+
+          <Button htmlType="button" additionalClasses="btn-lg btn-block">
+            Create account
+          </Button>
+
+          <HelperParagraph
+            helperText="Back to"
+            linkTo="/login"
+            boldText="login"
+          />
+        </form>
+
+        <Footer />
+      </div>
+    </main>
   );
 }
 
