78955432c931187a2fe86d150369bef933a1f16f
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-26 13:22:02 +0100

    Create reusable component Button

---
+++ client/src/components/Button/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+import cn from "classnames";
+
+import "./Button.scss";
+
+function Button({
+  children,
+  htmlType = "button",
+  additionalClasses,
+  ...props
+}) {
+  const classes = cn("btn", additionalClasses && additionalClasses.split(" "));
+
+  return (
+    <button className={classes} type={htmlType} {...props}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
