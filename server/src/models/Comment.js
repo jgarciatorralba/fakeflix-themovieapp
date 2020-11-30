13709d8175344828ed1b1b84560ea3574ebfa29f
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-30 18:52:17 +0100

    Add timestamps to Comment model

--- server/src/models/Comment.js
+++ server/src/models/Comment.js
@@ -2,20 +2,25 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const CommentSchema = new mongoose.Schema({
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
+const CommentSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    movie_id: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      requred: true,
+    },
   },
-  movie_id: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    requred: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Comment = mongoose.model("Comment", CommentSchema);
 
