250c9106ce8aaf81d6589c30543d023a99065b56
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-30 08:47:45 +0100

    Complete design of view 'password forgot'

--- client/src/pages/ForgotPassword/ForgotPassword.js
+++ client/src/pages/ForgotPassword/ForgotPassword.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import Alert from "react-bootstrap/Alert";
+
+import Logo from "../../components/Logo/Logo";
+import Label from "../../components/Label/Label";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+import Footer from "../../components/Footer/Footer";
+import HelperParagraph from "../../components/HelperParagraph/HelperParagraph";
+
+import "./ForgotPassword.scss";
 
 function ForgotPassword() {
+  const [show, setShow] = useState(true);
+
   return (
-    <div className="ForgotPassword">
-      <h1>Password Forgot</h1>
+    <div className="cont-forgot">
+      <div className="text-center w-100">
+        <main>
+          <Logo fontSize="2.75rem" />
+
+          <form className="form-forgot" autoComplete="off">
+            <h1 className="h3 font-weight-normal">Forgot your password?</h1>
+            <p className="mb-3">Enter your email to restore it.</p>
+
+            <Label htmlFor="forgotEmail">Email address</Label>
+            <Input
+              htmlType="email"
+              id="forgotEmail"
+              name="forgotEmail"
+              additionalClasses="single"
+              placeholder="Email address"
+              required
+            />
+
+            {show && (
+              <Alert
+                show={true}
+                onClose={() => setShow(false)}
+                variant="danger"
+                closeLabel="Close Alert"
+                dismissible
+                fade="true"
+              >
+                Dismissible alert!
+              </Alert>
+            )}
+
+            <Button htmlType="button" additionalClasses="btn-lg btn-block">
+              Send email
+            </Button>
+
+            <HelperParagraph
+              helperText="Back to"
+              linkTo="/login"
+              boldText="login"
+            />
+          </form>
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
