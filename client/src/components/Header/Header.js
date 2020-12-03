e48e0bcb8f895dd63f64ba76796578c31a8bdfa2
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-03 21:07:47 +0100

    Add props to Hader to dynamically update navbar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,13 @@ import Logo from "../../components/Logo/Logo";
 
 import "./Header.scss";
 
-function Header() {
+function Header({
+  isLoggingOut,
+  currentUser,
+  logoutError,
+  logoutSuccess,
+  logout,
+}) {
   return (
     <div className="Header">
       <Navbar sticky="top" bg="black" variant="dark" expand="lg">
@@ -18,9 +24,13 @@ function Header() {
           alt="Profile"
         />
         <NavDropdown title="" id="basic-nav-dropdown">
-          <NavDropdown.Item className="username">jorgartor</NavDropdown.Item>
+          <NavDropdown.Item className="username">
+            {currentUser.username}
+          </NavDropdown.Item>
           <NavDropdown.Item href="">Profile</NavDropdown.Item>
-          <NavDropdown.Item href="">Logout</NavDropdown.Item>
+          <NavDropdown.Item onClick={logout} href="">
+            Logout
+          </NavDropdown.Item>
         </NavDropdown>
       </Navbar>
     </div>
