c369d508b37360137760d28f42aa03cacd210103
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-04 19:56:14 +0100

    Add props and method to 'RandomMovieContainer'

--- client/src/redux/containers/components/RandomMovieContainer.js
+++ client/src/redux/containers/components/RandomMovieContainer.js
@@ -1,11 +1,18 @@
 import { connect } from "react-redux";
 
-import {} from "../../movies/movies-actions";
+import { fetchRandomMovie } from "../../movies/movies-actions";
 
 import RandomMovie from "../../../components/RandomMovie/RandomMovie";
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  randomMovieLoading: state.movies.randomMovieLoading,
+  randomMovieLoadingError: state.movies.randomMovieLoadingError,
+  randomMovieFetched: state.movies.randomMovieFetched,
+  randomMovie: state.movies.randomMovie,
+});
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  fetchRandomMovie: () => dispatch(fetchRandomMovie()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(RandomMovie);
