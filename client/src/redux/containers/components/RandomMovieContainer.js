ce97dfa5fc478d01e8c00b5c02a0fd9c0d159a34
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-04 16:39:41 +0100

    Create 'RandomMovieContainer' to connect state to component

---
+++ client/src/redux/containers/components/RandomMovieContainer.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux";
+
+import {} from "../../movies/movies-actions";
+
+import RandomMovie from "../../../components/RandomMovie/RandomMovie";
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RandomMovie);
