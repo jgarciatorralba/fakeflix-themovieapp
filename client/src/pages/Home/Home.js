9dd061eebd8e3fc00e9f0f4bdd86af30e0269cec
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-04 11:06:01 +0100

    Add 'isAuthenticated' to 'Home' component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+
+import ROUTES from "../../utils/routes";
 
 import HeaderContainer from "../../redux/containers/components/HeaderContainer";
 
 import "./Home.scss";
 
-function Home() {
+function Home({ isAuthenticated }) {
+  if (!isAuthenticated) {
+    return <Redirect to={ROUTES.LOGIN} />;
+  }
+
   return (
     <div className="Home">
       <HeaderContainer />
