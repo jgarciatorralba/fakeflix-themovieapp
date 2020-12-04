fdf9159b47faf3d929f39958d991ffd3da23b3ce
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-04 13:45:08 +0100

    Apply state management to 'ForgotPassword' component

--- client/src/pages/ForgotPassword/ForgotPassword.js
+++ client/src/pages/ForgotPassword/ForgotPassword.js
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+
+import ROUTES from "../../utils/routes";
+
 import Alert from "react-bootstrap/Alert";
 
 import Logo from "../../components/Logo/Logo";
@@ -10,8 +14,27 @@ import HelperParagraph from "../../components/HelperParagraph/HelperParagraph";
 
 import "./ForgotPassword.scss";
 
-function ForgotPassword() {
-  const [show, setShow] = useState(true);
+function ForgotPassword({
+  isAuthenticated,
+  isRequestingNewPassword,
+  forgotPassword,
+  forgetPasswordError,
+  forgetPasswordSuccess,
+  resetMessages,
+}) {
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email !== "") {
+      forgotPassword({ email });
+    }
+  }
+
+  if (isAuthenticated) {
+    return <Redirect to={ROUTES.HOME} />;
+  }
 
   return (
     <div className="cont-forgot">
@@ -19,7 +42,11 @@ function ForgotPassword() {
         <main>
           <Logo fontSize="2.75rem" />
 
-          <form className="form-forgot" autoComplete="off">
+          <form
+            className="form-forgot"
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
             <h1 className="h3 font-weight-normal">Forgot your password?</h1>
             <p className="mb-3">Enter your email to restore it.</p>
 
@@ -32,22 +59,41 @@ function ForgotPassword() {
               placeholder="Email address"
               required
               autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
-            {show && (
+            {forgetPasswordError && (
               <Alert
                 show={true}
-                onClose={() => setShow(false)}
+                onClose={resetMessages}
                 variant="danger"
                 closeLabel="Close Alert"
                 dismissible
                 fade="true"
               >
-                Dismissible alert!
+                {forgetPasswordError}
               </Alert>
             )}
 
-            <Button htmlType="button" additionalClasses="btn-lg btn-block">
+            {forgetPasswordSuccess && (
+              <Alert
+                show={true}
+                onClose={resetMessages}
+                variant="success"
+                closeLabel="Close Alert"
+                dismissible
+                fade="true"
+              >
+                {forgetPasswordSuccess}
+              </Alert>
+            )}
+
+            <Button
+              htmlType="submit"
+              additionalClasses="btn-lg btn-block"
+              disabled={isRequestingNewPassword}
+            >
               Send email
             </Button>
 
