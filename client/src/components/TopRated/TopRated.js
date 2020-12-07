03523655d1d4c06464e9abc47163b2cdecf97ca6
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-07 12:22:29 +0100

    Add basic HTML structure and styles

--- client/src/components/TopRated/TopRated.js
+++ client/src/components/TopRated/TopRated.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./TopRated.scss";
 
 function TopRated() {
-  return <div>Hola</div>;
+  return (
+    <div className="TopRated mt-1 mt-sm-3 px-3">
+      <h5 className="section-title">Top Rated</h5>
+      <div className="movie-cont py-3"></div>
+    </div>
+  );
 }
 
 export default TopRated;
