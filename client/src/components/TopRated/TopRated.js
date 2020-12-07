2330c09d5845979fed9604195b10d5733b808d8a
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-07 21:03:02 +0100

    Add 'useEffect' with timeout to hide movie cards printout

--- client/src/components/TopRated/TopRated.js
+++ client/src/components/TopRated/TopRated.js
@@ -16,6 +16,13 @@ function TopRated({
     fetchTopRated(page);
   }, [fetchTopRated, page]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      let hiderContainer = document.querySelector(".hider-cont");
+      hiderContainer.classList.remove("invisible");
+    }, 700);
+  });
+
   return (
     <div className="TopRated">
       <div className="mt-3 mt-sm-3 px-3">
@@ -40,67 +47,69 @@ function TopRated({
 
           {!topRatedMoviesLoadingError && !topRatedMoviesLoading && (
             <React.Fragment>
-              <div className="next-prev-cont">
-                <button
-                  className="btn-prev"
-                  type="button"
-                  onClick={() => setPage(page - 1)}
-                  disabled={page === 1}
-                >
-                  <svg
-                    width="1.5rem"
-                    height="1.5rem"
-                    viewBox="0 0 16 16"
-                    className="bi bi-arrow-left-circle prev-sign mr-1"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
+              <div className="hider-cont invisible">
+                <div className="next-prev-cont">
+                  <button
+                    className="btn-prev"
+                    type="button"
+                    onClick={() => setPage(page - 1)}
+                    disabled={page === 1}
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
-                    />
-                  </svg>
-                </button>
-              </div>
-              {topRatedMovies.length > 0 &&
-                topRatedMovies.map((movie) => (
-                  <Link
-                    key={movie.id}
-                    to={`/movie/${movie.id}`}
-                    className="movie-item mx-1"
+                    <svg
+                      width="1.5rem"
+                      height="1.5rem"
+                      viewBox="0 0 16 16"
+                      className="bi bi-arrow-left-circle prev-sign mr-1"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+                      />
+                      <path
+                        fillRule="evenodd"
+                        d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                {topRatedMovies.length > 0 &&
+                  topRatedMovies.map((movie) => (
+                    <Link
+                      key={movie.id}
+                      to={`/movie/${movie.id}`}
+                      className="movie-item mx-1"
+                    >
+                      <img src={movie.poster_path} alt="Movie poster" />
+                    </Link>
+                  ))}
+                <div className="next-prev-cont">
+                  <button
+                    className="btn-next"
+                    type="button"
+                    onClick={() => setPage(page + 1)}
+                    disabled={page === topRatedPages}
                   >
-                    <img src={movie.poster_path} alt="Movie poster" />
-                  </Link>
-                ))}
-              <div className="next-prev-cont">
-                <button
-                  className="btn-next"
-                  type="button"
-                  onClick={() => setPage(page + 1)}
-                  disabled={page === topRatedPages}
-                >
-                  <svg
-                    width="1.5rem"
-                    height="1.5rem"
-                    viewBox="0 0 16 16"
-                    className="bi bi-arrow-right-circle ml-1"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      d="M4 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5A.5.5 0 0 0 4 8z"
-                    />
-                  </svg>
-                </button>
+                    <svg
+                      width="1.5rem"
+                      height="1.5rem"
+                      viewBox="0 0 16 16"
+                      className="bi bi-arrow-right-circle ml-1"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+                      />
+                      <path
+                        fillRule="evenodd"
+                        d="M4 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5A.5.5 0 0 0 4 8z"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             </React.Fragment>
           )}
