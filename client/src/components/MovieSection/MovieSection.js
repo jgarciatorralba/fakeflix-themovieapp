9b730269439d5d3b804fa7ac12a93c3a40669174
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-08 13:25:53 +0100

    Change setTimeout selector to several containers, show prev button all the time

--- client/src/components/MovieSection/MovieSection.js
+++ client/src/components/MovieSection/MovieSection.js
@@ -20,9 +20,11 @@ function MovieSection({
 
   useEffect(() => {
     setTimeout(() => {
-      let hiderContainer = document.querySelector(".hider-cont");
-      if (hiderContainer) {
-        hiderContainer.classList.remove("invisible");
+      let hiderContainers = document.querySelectorAll(".hider-cont");
+      if (hiderContainers) {
+        hiderContainers.forEach((container) => {
+          container.classList.remove("invisible");
+        });
       }
     }, 700);
   });
@@ -64,41 +66,37 @@ function MovieSection({
           {!sectionLoadingError &&
             !sectionLoading &&
             (movies.length === 0 ? (
-              <div className="d-flex justify-content-center align-items-center m-1 w-100 mx-auto empty-cont">
-                <p className="p-3 my-0">
-                  Add movies to your favourites to display them here...
-                </p>
+              <div className="d-flex justify-content-center align-items-center m-1 w-100 mx-auto empty-cont rounded">
+                <p className="p-3 my-0">No movies added to favourites yet...</p>
               </div>
             ) : (
               <div className="hider-cont invisible">
-                {totalPages > 1 && (
-                  <div className="next-prev-cont">
-                    <button
-                      className="btn-prev"
-                      type="button"
-                      onClick={() => setPage(page - 1)}
-                      disabled={page === 1}
+                <div className="next-prev-cont">
+                  <button
+                    className="btn-prev"
+                    type="button"
+                    onClick={() => setPage(page - 1)}
+                    disabled={page === 1}
+                  >
+                    <svg
+                      width="1.5rem"
+                      height="1.5rem"
+                      viewBox="0 0 16 16"
+                      className="bi bi-arrow-left-circle prev-sign mr-1"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <svg
-                        width="1.5rem"
-                        height="1.5rem"
-                        viewBox="0 0 16 16"
-                        className="bi bi-arrow-left-circle prev-sign mr-1"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-                        />
-                        <path
-                          fillRule="evenodd"
-                          d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                )}
+                      <path
+                        fillRule="evenodd"
+                        d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+                      />
+                      <path
+                        fillRule="evenodd"
+                        d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
+                      />
+                    </svg>
+                  </button>
+                </div>
 
                 {movies.length > 0 &&
                   movies.map((movie) => (
