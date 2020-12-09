4b2ef661016c8ddf6cfd21d31f6856232b5b6d6e
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-09 16:01:22 +0100

    Create 'MovieContainer.js' to connect component with state

--- client/src/redux/containers/pages/MovieContainer.js
+++ client/src/redux/containers/pages/MovieContainer.js
@@ -1,11 +1,19 @@
 import { connect } from "react-redux";
 
+import { fetchMovieDetails } from "../../movies/movies-actions";
+
 import Movie from "../../../pages/Movie/Movie";
 
 const mapStateToProps = (state) => ({
   isAuthenticated: state.user.isAuthenticated,
+  movieDetailsLoading: state.movies.movieDetailsLoading,
+  movieDetailsLoadingError: state.movies.movieDetailsLoadingError,
+  movieDetailsFetched: state.movies.movieDetailsFetched,
+  movieDetails: state.movies.movieDetails,
 });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  fetchMovieDetails: (movie_id) => dispatch(fetchMovieDetails(movie_id)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Movie);
