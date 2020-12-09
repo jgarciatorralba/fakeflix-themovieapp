e38b980d9c67001fe53a1b1403601756908ac71b
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-09 11:38:13 +0100

    Add header and footer to component 'Movie'

--- client/src/pages/Movie/Movie.js
+++ client/src/pages/Movie/Movie.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { useParams } from "react-router";
+import { Redirect } from "react-router-dom";
 
-function Movie() {
+import ROUTES from "../../utils/routes";
+
+import HeaderContainer from "../../redux/containers/components/HeaderContainer";
+import Footer from "../../components/Footer/Footer";
+
+import "./Movie.scss";
+
+function Movie({ isAuthenticated }) {
   let { movie_id } = useParams();
+
+  if (!isAuthenticated) {
+    return <Redirect to={ROUTES.LOGIN} />;
+  }
 
   return (
     <div className="Movie">
+      <HeaderContainer />
       <h1>Movie {movie_id}</h1>
+      <Footer />
     </div>
   );
 }
