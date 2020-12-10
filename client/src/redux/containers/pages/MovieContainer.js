c21308a12a96ad64b02cea8fa38735e5535f76d8
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-10 15:22:14 +0100

    Add favourite-related props and methods to 'MovieContainer'

--- client/src/redux/containers/pages/MovieContainer.js
+++ client/src/redux/containers/pages/MovieContainer.js
@@ -1,6 +1,11 @@
 import { connect } from "react-redux";
 
-import { fetchMovieDetails } from "../../movies/movies-actions";
+import {
+  fetchMovieDetails,
+  fetchFavourites,
+  addFavourite,
+  removeFavourite,
+} from "../../movies/movies-actions";
 
 import Movie from "../../../pages/Movie/Movie";
 
@@ -10,10 +15,20 @@ const mapStateToProps = (state) => ({
   movieDetailsLoadingError: state.movies.movieDetailsLoadingError,
   movieDetailsFetched: state.movies.movieDetailsFetched,
   movieDetails: state.movies.movieDetails,
+
+  favouriteMovies: state.movies.favouriteMovies,
+  favouriteUpdating: state.movies.favouriteUpdating,
+  favouriteUpdatingError: state.movies.favouriteUpdatingError,
+  favouriteAdded: state.movies.favouriteAdded,
+  favouriteRemoved: state.movies.favouriteRemoved,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   fetchMovieDetails: (movie_id) => dispatch(fetchMovieDetails(movie_id)),
+
+  fetchFavourites: (movie_id) => dispatch(fetchFavourites(movie_id)),
+  addFavourite: (movie_id) => dispatch(addFavourite(movie_id)),
+  removeFavourite: (movie_id) => dispatch(removeFavourite(movie_id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Movie);
