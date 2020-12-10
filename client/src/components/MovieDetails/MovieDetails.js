2dc43a8963974b119d78c739fa41b8b66646f392
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-10 22:56:05 +0100

    Remove useState used for likes/dislikes in 'MovieDetails'

--- client/src/components/MovieDetails/MovieDetails.js
+++ client/src/components/MovieDetails/MovieDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import moment from "moment";
 
@@ -35,12 +35,6 @@ function MovieDetails({
 }) {
   const releaseDate = new moment(details.release_date).format("MMMM Do, YYYY");
 
-  let likeCount = likes.likeCount;
-  const [numLikes, setNumLikes] = useState(likeCount);
-
-  let dislikeCount = dislikes.dislikeCount;
-  const [numDislikes, setNumDislikes] = useState(dislikeCount);
-
   let movieIsInFavs;
   let favBtnActive = "";
   if (Object.entries(details).length > 0) {
@@ -65,14 +59,11 @@ function MovieDetails({
   function handleLike(e) {
     if (e.currentTarget.classList.contains("active")) {
       removeLike(details.id);
-      setNumLikes(numLikes - 1);
     } else {
       addLike(details.id);
-      setNumLikes(numLikes + 1);
       const dislikeBtn = document.querySelector(".btn-dislike");
       if (dislikeBtn.classList.contains("active")) {
         dislikeBtn.classList.remove("active");
-        setNumDislikes(numDislikes - 1);
       }
     }
     if (likeAdded || likeRemoved) {
@@ -84,14 +75,11 @@ function MovieDetails({
   function handleDislike(e) {
     if (e.currentTarget.classList.contains("active")) {
       removeDislike(details.id);
-      setNumDislikes(numDislikes - 1);
     } else {
       addDislike(details.id);
-      setNumDislikes(numDislikes + 1);
       const likeBtn = document.querySelector(".btn-like");
       if (likeBtn.classList.contains("active")) {
         likeBtn.classList.remove("active");
-        setNumLikes(numLikes - 1);
       }
     }
     if (dislikeAdded || dislikeRemoved) {
@@ -162,7 +150,7 @@ function MovieDetails({
                     </button>
                     <div className="d-flex justify-content-between align-items-center mt-1 mt-sm-2 mt-lg-3">
                       <div className="likes">
-                        <p className="my-0 text-center">{numLikes}</p>
+                        <p className="my-0 text-center">{likes.likeCount}</p>
                         <button
                           className={
                             (likes.likedByUser ? "active" : "") +
@@ -188,7 +176,9 @@ function MovieDetails({
                       </div>
 
                       <div className="dislikes">
-                        <p className="my-0 text-center">{numDislikes}</p>
+                        <p className="my-0 text-center">
+                          {dislikes.dislikeCount}
+                        </p>
                         <button
                           className={
                             (dislikes.dislikedByUser ? "active" : "") +
