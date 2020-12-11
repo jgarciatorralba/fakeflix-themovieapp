eca84f3f13a6fbd950b8b6931966e28c7eb2ce51
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-11 13:58:40 +0100

    Create new component 'MovieTrailers.js' and stylesheet

---
+++ client/src/components/MovieTrailers/MovieTrailers.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./MovieTrailers.scss";
+
+function MovieTrailers({
+  movieTrailers,
+  movieTrailersLoading,
+  movieTrailersLoadingError,
+}) {
+  console.log(movieTrailers);
+
+  return (
+    <div className="MovieTrailers">
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default MovieTrailers;
