09d5be28e9ce2c69a51dcaf7e762c421db0b8420
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-12 21:46:18 +0100

    Auto-scroll to top of the window on 'MovieDetails'

--- client/src/components/MovieDetails/MovieDetails.js
+++ client/src/components/MovieDetails/MovieDetails.js
@@ -33,6 +33,8 @@ function MovieDetails({
   dislikeAdded,
   dislikeRemoved,
 }) {
+  window.scrollTo(0, 0);
+
   const releaseDate = new moment(details.release_date).format("MMMM Do, YYYY");
   const genres = details.genres;
   const production_countries = details.production_countries;
