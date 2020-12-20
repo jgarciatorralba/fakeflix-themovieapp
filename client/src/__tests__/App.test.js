8baf63b4e1d3ada9bd3c74414955e24aa7ab217a
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-20 11:15:55 +0100

    First demo integration test at 'App.test.js'

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from ".././redux/store";
+import App from "../App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/please sign in/i);
   expect(linkElement).toBeInTheDocument();
 });
