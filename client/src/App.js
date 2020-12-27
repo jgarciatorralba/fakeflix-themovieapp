a31b15b35bcc7306d27eec18c7a6469e9ae9b169
jgarciatorralba <jgarciatorralba@gmail.com>
2020-12-27 16:22:03 +0100

    Import and use component "ScrollToTop" in "App.js"

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import ROUTES from "./utils/routes";
+import ScrollToTop from "./utils/ScrollToTop";
 
 import RegisterContainer from "./redux/containers/pages/RegisterContainer";
 import LoginContainer from "./redux/containers/pages/LoginContainer";
@@ -11,32 +12,36 @@ import ResetPasswordContainer from "./redux/containers/pages/ResetPasswordContai
 import HomeContainer from "./redux/containers/pages/HomeContainer";
 import MovieContainer from "./redux/containers/pages/MovieContainer";
 import NotFound from "./pages/NotFound/NotFound";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <Switch>
-      <Route path={ROUTES.LOGIN}>
-        <LoginContainer />
-      </Route>
-      <Route path={ROUTES.REGISTER}>
-        <RegisterContainer />
-      </Route>
-      <Route path={ROUTES.PASS_FORGOT}>
-        <ForgotPasswordContainer />
-      </Route>
-      <Route path={ROUTES.PASS_RESET}>
-        <ResetPasswordContainer />
-      </Route>
-      <Route path={ROUTES.MOVIE}>
-        <MovieContainer />
-      </Route>
-      <Route path={ROUTES.HOME} exact>
-        <HomeContainer />
-      </Route>
-      <Route>
-        <NotFound />
-      </Route>
-    </Switch>
+    <Fragment>
+      <ScrollToTop />
+      <Switch>
+        <Route path={ROUTES.LOGIN}>
+          <LoginContainer />
+        </Route>
+        <Route path={ROUTES.REGISTER}>
+          <RegisterContainer />
+        </Route>
+        <Route path={ROUTES.PASS_FORGOT}>
+          <ForgotPasswordContainer />
+        </Route>
+        <Route path={ROUTES.PASS_RESET}>
+          <ResetPasswordContainer />
+        </Route>
+        <Route path={ROUTES.MOVIE}>
+          <MovieContainer />
+        </Route>
+        <Route path={ROUTES.HOME} exact>
+          <HomeContainer />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Fragment>
   );
 }
 
