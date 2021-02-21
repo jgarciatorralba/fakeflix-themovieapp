30bd7b2aac683bb1de91f5655f4125531b8c0b8d
jgarciatorralba <jgarciatorralba@gmail.com>
2021-02-21 19:38:00 +0100

    Add "useState" to toggle active options

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
 import ROUTES from "../../utils/routes";
@@ -8,6 +8,19 @@ import HeaderContainer from "../../redux/containers/components/HeaderContainer";
 import "./Profile.scss";
 
 function Profile({ isAuthenticated }) {
+  const [sectionDetails, setSectionDetails] = useState("active-option");
+  const [sectionPassword, setSectionPassword] = useState("");
+
+  function handleClick(e) {
+    if (e.target.innerText === "Contact details") {
+      setSectionDetails("active-option");
+      setSectionPassword("");
+    } else if (e.target.innerText === "Password") {
+      setSectionDetails("");
+      setSectionPassword("active-option");
+    }
+  }
+
   if (!isAuthenticated) {
     return <Redirect to={ROUTES.HOME} />;
   }
@@ -16,7 +29,25 @@ function Profile({ isAuthenticated }) {
     <div className="Profile">
       <HeaderContainer />
 
-      <h1>Profile</h1>
+      <h5 className="p-3 mx-auto">Your Profile</h5>
+      <div className="cont-profile-options mx-3 mb-3 ">
+        <ul className="p-0">
+          <li
+            className={`d-inline py-3 mr-3 ${sectionDetails}`}
+            onClick={(e) => handleClick(e)}
+          >
+            Contact details
+          </li>
+          <li
+            className={`d-inline py-3 mr-3 ${sectionPassword}`}
+            onClick={(e) => handleClick(e)}
+          >
+            Password
+          </li>
+        </ul>
+      </div>
+      {sectionDetails !== "" && <div>Contact</div>}
+      {sectionPassword !== "" && <div>Password</div>}
     </div>
   );
 }
