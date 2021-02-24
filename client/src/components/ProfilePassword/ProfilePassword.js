b66bfd1e012095fe298094010c0e75073f9cf8b0
jgarciatorralba <jgarciatorralba@gmail.com>
2021-02-24 21:19:07 +0100

    Add React logic to handle form (pending Redux)

--- client/src/components/ProfilePassword/ProfilePassword.js
+++ client/src/components/ProfilePassword/ProfilePassword.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
@@ -6,11 +6,40 @@ import Alert from "react-bootstrap/Alert";
 
 import "./ProfilePassword.scss";
 
-function ProfilePassword({ currentUser }) {
-  console.log(currentUser);
+function ProfilePassword({ resetMessages }) {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [newPasswordsError, setNewPasswordsError] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (
+      currentPassword !== "" &&
+      newPassword !== "" &&
+      confirmPassword !== ""
+    ) {
+      if (newPassword !== confirmPassword) {
+        setNewPasswordsError("New passwords do not match!");
+      } else {
+        /* TODO: Check current password in database.
+         ** If correct, update new password and show confirmation message,
+         ** else show error message.
+         */
+
+        // * To be deleted
+        setNewPasswordsError("Fine!");
+      }
+    }
+  }
 
   return (
-    <form className="form-profile-password" autoComplete="off">
+    <form
+      className="form-profile-password"
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
       <label htmlFor="profileCurrentPassword" className="px-4 px-sm-5">
         <b>Current Password</b>
       </label>
@@ -20,6 +49,9 @@ function ProfilePassword({ currentUser }) {
           id="profileCurrentPassword"
           name="profileCurrentPassword"
           additionalClasses="single"
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.target.value)}
+          required
           autoFocus
         />
       </div>
@@ -33,6 +65,10 @@ function ProfilePassword({ currentUser }) {
           id="profileNewPassword"
           name="profileNewPassword"
           additionalClasses="single"
+          minLength="6"
+          required
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
         />
       </div>
 
@@ -45,12 +81,29 @@ function ProfilePassword({ currentUser }) {
           id="profileRepeatPassword"
           name="profileRepeatPassword"
           additionalClasses="single"
+          required
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
       </div>
 
-      {/* Alert message here */}
-
       <span>&nbsp;</span>
+      {/* Alert messages here */}
+
+      {newPasswordsError !== "" && (
+        <div className="alert-wrapper mx-4 mx-sm-5">
+          <Alert
+            show={true}
+            onClose={() => setNewPasswordsError("")}
+            variant="danger"
+            closeLabel="Close Alert"
+            dismissible
+            fade="true"
+          >
+            {newPasswordsError}
+          </Alert>
+        </div>
+      )}
       <div className="input-group px-4 px-sm-5 mb-2 mb-sm-5">
         <Button htmlType="submit" additionalClasses="btn-lg btn-block">
           Save
