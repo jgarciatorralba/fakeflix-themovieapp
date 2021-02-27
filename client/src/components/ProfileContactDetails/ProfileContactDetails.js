c1641b9076197be3131b62c3063571bb3d1058cc
jgarciatorralba <jgarciatorralba@gmail.com>
2021-02-27 17:22:40 +0100

    Add state management for component 'ProfileContactDetails'

--- client/src/components/ProfileContactDetails/ProfileContactDetails.js
+++ client/src/components/ProfileContactDetails/ProfileContactDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
@@ -6,11 +6,49 @@ import Alert from "react-bootstrap/Alert";
 
 import "./ProfileContactDetails.scss";
 
-function ProfileContactDetails({ currentUser }) {
+function ProfileContactDetails({
+  currentUser,
+  isUpdatingProfile,
+  updateProfileError,
+  updateProfileSuccess,
+  updateContact,
+  resetMessages,
+}) {
+  const [username, setUsername] = useState(currentUser.username);
+  const [email, setEmail] = useState(currentUser.email);
+
   console.log(currentUser);
 
+  useEffect(() => {
+    resetMessages();
+  }, [resetMessages]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (username !== "" && email !== "") {
+      if (username === currentUser.username && email !== currentUser.email) {
+        updateContact({ undefined, email });
+      } else if (
+        username !== currentUser.username &&
+        email === currentUser.email
+      ) {
+        updateContact({ username, undefined });
+      } else if (
+        username !== currentUser.username &&
+        email !== currentUser.email
+      ) {
+        updateContact({ username, email });
+      }
+    }
+  }
+
   return (
-    <form className="form-profile-contact-details" autoComplete="off">
+    <form
+      className="form-profile-contact-details"
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
       <label htmlFor="profileUsername" className="px-4 px-sm-5">
         <b>Username</b>
       </label>
@@ -20,6 +58,10 @@ function ProfileContactDetails({ currentUser }) {
           id="profileUsername"
           name="profileUsername"
           additionalClasses="single"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          spellCheck="false"
           autoFocus
         />
       </div>
@@ -33,14 +75,54 @@ function ProfileContactDetails({ currentUser }) {
           id="profileEmail"
           name="profileEmail"
           additionalClasses="single"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          spellCheck="false"
+          required
         />
       </div>
 
-      {/* Alert message here */}
-
       <span>&nbsp;</span>
+
+      {updateProfileError && (
+        <div className="alert-wrapper mx-4 mx-sm-5">
+          <Alert
+            show={true}
+            onClose={resetMessages}
+            variant="danger"
+            closeLabel="Close Alert"
+            dismissible
+            fade="true"
+          >
+            {updateProfileError}
+          </Alert>
+        </div>
+      )}
+
+      {updateProfileSuccess && (
+        <div className="alert-wrapper mx-4 mx-sm-5">
+          <Alert
+            show={true}
+            onClose={resetMessages}
+            variant="success"
+            closeLabel="Close Alert"
+            dismissible
+            fade="true"
+          >
+            {updateProfileSuccess}
+          </Alert>
+        </div>
+      )}
+
       <div className="input-group px-4 px-sm-5 mb-2 mb-sm-5">
-        <Button htmlType="submit" additionalClasses="btn-lg btn-block">
+        <Button
+          htmlType="submit"
+          additionalClasses="btn-lg btn-block"
+          disabled={
+            isUpdatingProfile ||
+            (username === currentUser.username && email === currentUser.email)
+          }
+        >
           Update
         </Button>
       </div>
