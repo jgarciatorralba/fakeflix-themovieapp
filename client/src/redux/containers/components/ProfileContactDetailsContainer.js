0c454d608661de737f791919ce41f12dd7b756da
jgarciatorralba <jgarciatorralba@gmail.com>
2021-02-27 17:20:05 +0100

    Map state to props for contact details

--- client/src/redux/containers/components/ProfileContactDetailsContainer.js
+++ client/src/redux/containers/components/ProfileContactDetailsContainer.js
@@ -1,12 +1,21 @@
 import { connect } from "react-redux";
 
+import { updateContact, resetMessages } from "../../user/user-actions";
+
 import ProfileContactDetails from "../../../components/ProfileContactDetails/ProfileContactDetails";
 
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
+  isUpdatingProfile: state.user.isUpdatingProfile,
+  updateProfileError: state.user.updateProfileError,
+  updateProfileSuccess: state.user.updateProfileSuccess,
 });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  updateContact: ({ username, email }) =>
+    dispatch(updateContact({ username, email })),
+  resetMessages: () => dispatch(resetMessages()),
+});
 
 export default connect(
   mapStateToProps,
