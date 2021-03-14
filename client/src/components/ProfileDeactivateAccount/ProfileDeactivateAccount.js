f7783143e114f11733eefeba4ebbc1fd29515f52
jgarciatorralba <jgarciatorralba@gmail.com>
2021-03-14 21:36:35 +0100

    Complete basic styling for "Delete account" section

--- client/src/components/ProfileDeactivateAccount/ProfileDeactivateAccount.js
+++ client/src/components/ProfileDeactivateAccount/ProfileDeactivateAccount.js
@@ -1,9 +1,32 @@
 import React from "react";
 
+import Button from "../../components/Button/Button";
+
 import "./ProfileDeactivateAccount.scss";
 
 function ProfileDeactivateAccount() {
-  return <div>Deactivate Account</div>;
+  return (
+    <div className="cont-profile-deactivate-account">
+      <h6 className="px-4 px-sm-5">
+        <b>Delete account</b>
+      </h6>
+      <p className="px-4 px-sm-5">
+        This process is permanent and cannot be undone.
+      </p>
+
+      <span>&nbsp;</span>
+
+      <div className="input-group px-4 px-sm-5 mb-2 mb-sm-5">
+        <Button
+          htmlType="submit"
+          id="btnDeactivate"
+          additionalClasses="btn-lg btn-block"
+        >
+          Confirm
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileDeactivateAccount;
